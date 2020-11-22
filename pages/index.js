e66f80636b68ed6c0638f32da58b788c0697024b
Abhishek Kumar <abhishek@zomentum.com>
2020-11-22 22:18:11 +0530

    fix: initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,95 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const skills = [
+        {
+            skillName: "JavaScript",
+            iconPath: "/javascript.png",
+        },
+        {
+            skillName: "TypeScript",
+            iconPath: "/typescript.png",
+        },
+        {
+            skillName: "React",
+            iconPath: "/react.png",
+        },
+        {
+            skillName: "Redux",
+            iconPath: "/redux.png",
+        },
+        {
+            skillName: "Firebase",
+            iconPath: "/firebase.png",
+        },
+    ];
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+    return (
+        <div className="container mx-auto grid grid-cols-12 text-blue-400 font-title">
+            <Head>
+                <title>Abhishek Kumar</title>
+            </Head>
+            <div className="mt-16 col-span-full">
+                <div className=" text-xl pb-5">Hey </div>
+                <div className="text-2xl font-semibold text-blue-500">
+                    I'm Abhishek kumar
+                </div>
+            </div>
+            <p className="col-start-1 col-end-8 mt-10 text-xl font-montserrat">
+                I'm a Frontend Web Developer, currently located in Bangalore.
             </p>
-          </a>
+            <p className="col-start-1 col-end-8 mt-10 text-xl">
+                Developer at{" "}
+                <a
+                    href="https://zomentum.com"
+                    target="__blank"
+                    className=" hover:text-blue-600 hover:underline font-semibold leading-3 mx-1 text-blue-500"
+                >
+                    Zomentum
+                </a>
+            </p>
+            <div className="mt-10 col-span-full text-xl font-semibold">
+                I know ⤵
+            </div>
+            <div className="mt-5 flex flex-wrap col-start-1 col-end-8">
+                {skills.map((skill, index) => (
+                    <div
+                        key={index}
+                        className={`grid place-items-center mb-2 mr-8`}
+                    >
+                        <img width={56} height={56} src={skill.iconPath} />
+                        <span className="text-blue-500 text-md mt-2">
+                            {skill.skillName}
+                        </span>
+                    </div>
+                ))}
+            </div>
+            <div className="col-span-full my-6">
+                I also dabble in NodeJS and Express 😛{" "}
+            </div>
+            <div className="col-span-full">
+                You can reach me at{" "}
+                <a
+                    href=""
+                    className="hover:underline font-semibold text-blue-500"
+                >
+                    abhishekkumar35962@gmail.com
+                </a>
+            </div>
+            <footer className="mt-10 flex col-span-1">
+                <a target="__blank" href="https://github.com/Abhikumar98">
+                    <img
+                        width={32}
+                        height={32}
+                        src="/github.png"
+                        className="mr-5"
+                    />
+                </a>
+                <a target="__blank" href="https://twitter.com/abhikumar_98">
+                    <img width={32} height={32} src="/twitter.png" />
+                </a>
+            </footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: [],
+    darkMode: false,
+    theme: {
+        container: {
+            center: true,
+        },
+        extend: {
+            fontFamily: {
+                title: ["Quicksand"],
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
