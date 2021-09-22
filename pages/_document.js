c8f27c305663d7257f7d22c55efd8fba1b6f1364
abhikumar98 <abhishekkumar35962@gmail.com>
2021-09-22 22:42:11 +0530

    fix: buy me a coffee integrated

--- pages/_document.js
+++ pages/_document.js
@@ -28,10 +28,29 @@ class MyDocument extends Document {
 										`,
                                 }}
                             />
+                            <script
+                                dangerouslySetInnerHTML={{
+                                    __html: `
+										
+										`,
+                                }}
+                            />
                         </>
                     )}
                 </Head>
                 <body>
+                    <script
+                        data-name="BMC-Widget"
+                        data-cfasync="false"
+                        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+                        data-id="abhishekkumar98"
+                        data-description="Support me on Buy me a coffee!"
+                        data-message=""
+                        data-color="#5F7FFF"
+                        data-position="Right"
+                        data-x_margin="18"
+                        data-y_margin="18"
+                    ></script>
                     <Main />
                     <NextScript />
                 </body>
